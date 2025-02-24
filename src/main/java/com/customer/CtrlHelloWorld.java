b31b0ec80b6916de21bd7644ce12af3d815ddf0d
Iván Saavedra <ivan.saavedra@people-media.com.mx>
2025-02-24 08:22:04 -0600

    Update CtrlHelloWorld.java

--- src/main/java/com/customer/CtrlHelloWorld.java
+++ src/main/java/com/customer/CtrlHelloWorld.java
@@ -13,40 +13,7 @@ public class CtrlHelloWorld {
 
 	@GetMapping
 	public String helloWorld() {
-		return "Hello World!";
-	}
-
-	@GetMapping("/category")
-	public List<Category> getCategories() {
-		List<Category> categories = new ArrayList();
-		categories.add(new Category(1,"Electrónica"));
-		categories.add(new Category(2,"Línea Blanca"));
-		return categories;
-	}
-	
-	private class Category{
-		private Integer id;
-		private String category;
-		
-		public Category(Integer id, String category) {
-			super();
-			this.id = id;
-			this.category = category;
-		}
-		public Integer getId() {
-			return id;
-		}
-		public void setId(Integer id) {
-			this.id = id;
-		}
-		public String getCategory() {
-			return category;
-		}
-		public void setCategory(String category) {
-			this.category = category;
-		}
-		
-		
+		return "Hello World";
 	}
 }
 
