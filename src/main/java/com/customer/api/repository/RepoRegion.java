6b45e604b1ab2a05775be2381ea0e57ddbb5b207
Jesús Ivan Saavedra Martinez <isaavedra@grupopm.mx>
2025-03-07 08:45:55 -0600

    feature region crud

--- src/main/java/com/customer/api/repository/RepoRegion.java
+++ src/main/java/com/customer/api/repository/RepoRegion.java
@@ -3,21 +3,50 @@ package com.customer.api.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.customer.api.entity.Region;
 
-@Repository
-public interface RepoRegion extends JpaRepository<Region, Integer>{
+import jakarta.transaction.Transactional;
 
-	@Query(value ="SELECT * FROM region ORDER BY region", nativeQuery = true)
+@Repository
+public interface RepoRegion extends JpaRepository<Region, Integer> {
+
+	@Query(value = "SELECT * FROM region ORDER BY region", nativeQuery = true)
 	List<Region> getRegions();
 
-	@Query(value ="SELECT * FROM region WHERE status = 1 ORDER BY region", nativeQuery = true)
+	@Query(value = "SELECT * FROM region WHERE status = 1 ORDER BY region", nativeQuery = true)
 	List<Region> getActiveRegions();
 
-	@Query(value ="SELECT * FROM region WHERE region_id = :region_id ORDER BY region", nativeQuery = true)
+	@Query(value = "SELECT * FROM region WHERE region_id = :region_id ORDER BY region", nativeQuery = true)
 	Region getRegion(@Param("region_id") Integer region_id);
+
+	@Modifying
+	@Transactional
+	@Query(value = "INSERT INTO region (region, tag, status) VALUES (:region, :tag, 1)", nativeQuery = true)
+	void createRegion(@Param("region") String region, @Param("tag") String tag);
+
+	@Modifying
+	@Transactional
+	@Query(value = "UPDATE region SET region = :region, tag = :tag WHERE region_id = :region_id;", nativeQuery = true)
+	void updateRegion(@Param("region_id") Integer region_id, @Param("region") String region, @Param("tag") String tag);
+
+	@Modifying
+	@Transactional
+	@Query(value = "UPDATE region SET status = :status WHERE region_id = :region_id;", nativeQuery = true)
+	void updateRegionStatus(@Param("region_id") Integer region_id, @Param("status") Integer status);
+
+	@Modifying
+	@Transactional
+	@Query(value = "UPDATE region SET status = 1 WHERE region_id = :region_id;", nativeQuery = true)
+	void enableRegion(@Param("region_id") Integer region_id);
+
+	@Modifying
+	@Transactional
+	@Query(value = "UPDATE region SET status = 0 WHERE region_id = :region_id;", nativeQuery = true)
+	void disableRegion(@Param("region_id") Integer region_id);
+
 }
--- src/main/java/com/customer/api/service/SvcRegionImp.java
+++ src/main/java/com/customer/api/service/SvcRegionImp.java
@@ -5,10 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.customer.api.dto.DtoRegionIn;
 import com.customer.api.entity.Region;
 import com.customer.api.repository.RepoRegion;
+import com.customer.common.ApiResponse;
 import com.customer.exception.ApiException;
 import com.customer.exception.DBAccessException;
 
@@ -19,31 +22,91 @@ public class SvcRegionImp implements SvcRegion {
 	RepoRegion repo;
 
 	@Override
-	public List<Region> getRegions() {
+	public ResponseEntity<List<Region>> getRegions() {
 		try {
-			return repo.getRegions();
+			return new ResponseEntity<>(repo.getRegions(), HttpStatus.OK);
 		}catch (DataAccessException e) {
 			throw new DBAccessException(e);
 		}
 	}
 
 	@Override
-	public List<Region> getActiveRegions() {
+	public ResponseEntity<List<Region>> getActiveRegions() {
 		try {
-			return repo.getActiveRegions();
+			return new ResponseEntity<>(repo.getActiveRegions(), HttpStatus.OK);
 		}catch (DataAccessException e) {
 			throw new DBAccessException(e);
 		}
 	}
 
 	@Override
-	public Region getRegion(Integer id) {
+	public ResponseEntity<Region> getRegion(Integer id) {
 		try {
-			Region region = repo.getRegion(id);
-			if(region == null) {
+			validateRegionId(id);
+			return new ResponseEntity<>(repo.getRegion(id), HttpStatus.OK);
+		}catch (DataAccessException e) {
+			throw new DBAccessException(e);
+		}
+	}
+
+	@Override
+	public ResponseEntity<ApiResponse> createRegion(DtoRegionIn in) {
+		try {
+			repo.createRegion(in.getRegion(), in.getTag());
+			return new ResponseEntity<>(new ApiResponse("La región ha sido registrada"), HttpStatus.CREATED);
+		}catch (DataAccessException e) {
+			if (e.getLocalizedMessage().contains("ux_region"))
+				throw new ApiException(HttpStatus.CONFLICT, "El nombre de la región ya está registrado");
+			if (e.getLocalizedMessage().contains("ux_tag"))
+				throw new ApiException(HttpStatus.CONFLICT, "El tag de la región ya está registrado");
+
+			throw new DBAccessException(e);
+		}
+	}
+
+	@Override
+	public ResponseEntity<ApiResponse> updateRegion(Integer id, DtoRegionIn in) {
+		try {
+			validateRegionId(id);
+			repo.updateRegion(id, in.getRegion(), in.getTag());
+			return new ResponseEntity<>(new ApiResponse("La región ha sido actualizada"), HttpStatus.OK);
+		}catch (DataAccessException e) {
+			if (e.getLocalizedMessage().contains("ux_region"))
+				throw new ApiException(HttpStatus.CONFLICT, "El nombre de la región ya está registrado");
+			if (e.getLocalizedMessage().contains("ux_tag"))
+				throw new ApiException(HttpStatus.CONFLICT, "El tag de la región ya está registrado");
+
+			throw new DBAccessException(e);
+		}
+	}
+
+	@Override
+	public ResponseEntity<ApiResponse> enableRegion(Integer id) {
+		try {
+			validateRegionId(id);
+			repo.enableRegion(id);
+			return new ResponseEntity<>(new ApiResponse("La región ha sido activada"), HttpStatus.OK);
+		}catch (DataAccessException e) {
+			throw new DBAccessException(e);
+		}
+	}
+
+	@Override
+	public ResponseEntity<ApiResponse> disableRegion(Integer id) {
+		try {
+			validateRegionId(id);
+			repo.disableRegion(id);
+			return new ResponseEntity<>(new ApiResponse("La región ha sido desactivada"), HttpStatus.OK);
+		}catch (DataAccessException e) {
+			throw new DBAccessException(e);
+		}
+	}
+	
+	private void validateRegionId(Integer id) {
+		try {
+			if(repo.getRegion(id) == null) {
 				throw new ApiException(HttpStatus.NOT_FOUND, "El id de la región no existe");
 			}
-			return region;
 		}catch (DataAccessException e) {
 			throw new DBAccessException(e);
 		}
